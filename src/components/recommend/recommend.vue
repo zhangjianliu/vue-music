b1302e37f2224050a00c4576119167d3d95988b3
zjlindd <3190547371@qq.com>
2018-01-13 12:15:31 +0800

    解决eslint空行问题

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -7,22 +7,6 @@
 
 <script type="text/ecmascript-6">
 
-
-  export default {
-    data() {
-      return {
-
-      }
-    },
-    created() {
-
-    },
-    methods: {
-    },
-    components: {
-
-    }
-  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
