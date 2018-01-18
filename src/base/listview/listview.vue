e87583cce936c894cbc0ad7b50631bc359db03f8
zjlindd <3190547371@qq.com>
2018-01-18 12:17:55 +0800

    歌手页面开发完成

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,245 @@
+<template>
+  <scroll @scroll="scroll"
+          :listen-scroll="listenScroll"
+          :probe-type="probeType"
+          :data="data"
+          class="listview"
+          ref="listview">
+    <ul>
+      <li v-for="group in data" class="list-group" ref="listGroup">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <uL>
+          <li @click="selectItem(item)" v-for="item in group.items" class="list-group-item">
+            <img class="avatar" v-lazy="item.avatar">
+            <span class="name">{{item.name}}</span>
+          </li>
+        </uL>
+      </li>
+    </ul>
+    <div class="list-shortcut" @touchstart.stop.prevent="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
+      <ul>
+        <li v-for="(item, index) in shortcutList" :data-index="index" class="item"
+            :class="{'current':currentIndex===index}">{{item}}
+        </li>
+      </ul>
+    </div>
+    <div class="list-fixed" ref="fixed" v-show="fixedTitle">
+      <div class="fixed-title">{{fixedTitle}} </div>
+    </div>
+    <div v-show="!data.length" class="loading-container">
+      <loading></loading>
+    </div>
+  </scroll>
+</template>
+
+<script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+  import {getData} from 'common/js/dom'
+
+  const TITLE_HEIGHT = 30  //
+  const ANCHOR_HEIGHT = 18 // 每个字母的高度是18
+
+  export default {
+    props: {
+      data: {
+        type: Array,
+        default: []
+      }
+    },
+    computed: {
+      shortcutList() {
+        return this.data.map((group) => {
+          return group.title.substr(0, 1)
+        })
+      },
+      fixedTitle() {
+        if (this.scrollY > 0) {
+          return ''
+        }
+        return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
+      }
+    },
+    data() {
+      return {
+        scrollY: -1,
+        currentIndex: 0,
+        diff: -1  //区块的上线-滚动的当前位置就是偏移差diff
+      }
+    },
+    created() {
+      //并不需要观测到变化，相反数据声明在data里面会添加setter和getter方法进行监听
+      this.probeType = 3
+      this.listenScroll = true
+      this.touch = {}
+      this.listHeight = []
+    },
+    methods: {
+      selectItem(item) {
+        this.$emit('select', item)
+      },
+      onShortcutTouchStart(e) {
+        let anchorIndex = getData(e.target, 'index')
+        let firstTouch = e.touches[0] //获取第一个手指的位置
+        this.touch.y1 = firstTouch.pageY //
+        this.touch.anchorIndex = anchorIndex //当前索引
+        this._scrollTo(anchorIndex)
+      },
+      onShortcutTouchMove(e) {
+        let firstTouch = e.touches[0] //获取第一个手指的位置
+        this.touch.y2 = firstTouch.pageY //
+        let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0 //相差几个索引
+
+        let anchorIndex = parseInt(this.touch.anchorIndex) + delta //计算当前应该滚动到的索引位置
+
+        this._scrollTo(anchorIndex)
+      },
+      refresh() {
+        this.$refs.listview.refresh()
+      },
+      //实时获取滚动到y轴的位置
+      scroll(pos) {
+        this.scrollY = pos.y
+      },
+      //计算各个module的叠加高度 放入数组中
+      _calculateHeight() {
+        this.listHeight = []
+        const list = this.$refs.listGroup
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < list.length; i++) {
+          let item = list[i]
+          height += item.clientHeight  //注意这个高度是累加的
+          this.listHeight.push(height)
+        }
+      },
+      //通过右侧快速入口来设置左侧滚动的位置
+      _scrollTo(index) {
+        if (!index && index !== 0) {
+          return
+        }
+        if (index < 0) {
+          index = 0
+        } else if (index > this.listHeight.length - 2) {
+          index = this.listHeight.length - 2
+        }
+        //通过 watch scrollY 来设置当前的index用来改变高亮的状态
+        this.scrollY = -this.listHeight[index]
+
+        this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+      }
+    },
+    watch: {
+      data() {
+        setTimeout(() => {
+          this._calculateHeight()
+        }, 20)
+      },
+      scrollY(newY) {
+        const listHeight = this.listHeight
+        //console.log('footer',listHeight[listHeight.length-1])
+        // 当滚动到顶部，newY>0
+        if (newY > 0) {
+          this.currentIndex = 0
+          return
+        }
+        // 在中间部分滚动
+        for (let i = 0; i < listHeight.length - 1; i++) {
+          // 注意 listHeight[0]===0
+          let height1 = listHeight[i]
+          let height2 = listHeight[i + 1]
+          if (-newY >= height1 && -newY < height2) {
+            this.currentIndex = i
+            this.diff = height2 + newY
+            return
+          }
+        }
+        // 当滚动到底部，且-newY大于最后一个元素的上限
+        this.currentIndex = listHeight.length - 2
+      },
+      diff(newVal) {
+        let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+        //减少dom 操作的频度 代码的优化
+        if (this.fixedTop === fixedTop) {
+          return
+        }
+        this.fixedTop = fixedTop
+        //console.log(this.fixedTop)
+        this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
+      }
+    },
+    components: {
+      Scroll,
+      Loading
+    }
+  }
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .listview
+    position: relative
+    width: 100%
+    height: 100%
+    overflow: hidden
+    background: $color-background
+    .list-group
+      padding-bottom: 30px
+      .list-group-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+      .list-group-item
+        display: flex
+        align-items: center
+        padding: 20px 0 0 30px
+        .avatar
+          width: 50px
+          height: 50px
+          border-radius: 50%
+        .name
+          margin-left: 20px
+          color: $color-text-l
+          font-size: $font-size-medium
+    .list-shortcut
+      position: absolute
+      z-index: 30
+      right: 0
+      top: 50%
+      transform: translateY(-50%)
+      width: 20px
+      padding: 20px 0
+      border-radius: 10px
+      text-align: center
+      background: $color-background-d
+      font-family: Helvetica
+      .item
+        padding: 3px
+        line-height: 1
+        color: $color-text-l
+        font-size: $font-size-small
+        &.current
+          color: $color-theme
+    .list-fixed
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      .fixed-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
+</style>
--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -13,32 +13,39 @@
         type: Number,
         default: 1
       },
+      /*点击列表是否派发click事件*/
       click: {
         type: Boolean,
         default: true
       },
+      /*是否派发滚动事件*/
       listenScroll: {
         type: Boolean,
         default: false
       },
+      /*列表的数据*/
       data: {
         type: Array,
         default: null
       },
+      /*是否派发滚动到底部的事件，用于上拉加载*/
       pullup: {
         type: Boolean,
         default: false
       },
+      /*是否派发列表滚动开始的事件*/
       beforeScroll: {
         type: Boolean,
         default: false
       },
+      /* 当数据更新后，刷新scroll的延时*/
       refreshDelay: {
         type: Number,
         default: 20
       }
     },
     methods: {
+      // better-scroll的初始化
       _initScroll () {
         if (!this.$refs.wrapper) {
           return
@@ -47,14 +54,14 @@
           probeType: this.probeType,
           click: this.click
         })
-
+        // 是否派发滚动事件  从父组件传递过来的
         if (this.listenScroll) {
           let me = this
           this.scroll.on('scroll', (pos) => {
-            me.$emit('scroll', pos)
+            me.$emit('scroll', pos)  //vue实例派发一个事件
           })
         }
-
+        // 是否派发滚动到底部事件，用于上拉加载
         if (this.pullup) {
           this.scroll.on('scrollEnd', () => {
             if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
@@ -69,22 +76,28 @@
         }
       },
       disable () {
+        // 代理better-scroll的disable方法
         this.scroll && this.scroll.disable()
       },
       enable () {
+        // 代理better-scroll的enable方法
         this.scroll && this.scroll.enable()
       },
       refresh () {
+        // 代理better-scroll的refresh方法
         this.scroll && this.scroll.refresh()
       },
       scrollTo () {
+        // 代理better-scroll的scrollTo方法
         this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
       },
       scrollToElement () {
+        // 代理better-scroll的scrollToElement方法
         this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
       }
     },
     mounted () {
+      // 保证在DOM渲染完毕后初始化better-scroll
       setTimeout(() => {
         this._initScroll()
       }, 20)
