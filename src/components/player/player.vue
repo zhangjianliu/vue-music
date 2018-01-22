854c2fbe3a040e267843e4313870358f075f80e4
zjlindd <3190547371@qq.com>
2018-01-22 17:50:02 +0800

    完成播放模式的切换，打乱数组，和从新设置currentIndex，不改变当前歌曲的播放

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -51,10 +51,10 @@
             <span class="time time-r">{{format(currentSong.duration)}}</span>
           </div>
           <div class="operators">
-            <div class="icon i-left" :class="disableCls">
-              <i class="icon-sequence" ></i>
+            <div class="icon i-left" >
+              <i :class="iconMode" @click="changeMode"></i>
             </div>
-            <div class="icon i-left">
+            <div class="icon i-left" :class="disableCls">
               <i class="icon-prev" @click="pre"></i>
             </div>
             <div class="icon i-center" :class="disableCls">
@@ -101,6 +101,7 @@
   import Scroll from 'base/scroll/scroll'
   import ProgressBar from 'base/progress-bar/progress-bar'
   import ProgressCircle from 'base/progress-circle/progress-circle'
+  import { setRandomArr } from 'common/js/util'
   const transform = prefixStyle('transform')
   const transitionDuration = prefixStyle('transitionDuration')
   export default {
@@ -131,12 +132,16 @@
       percent(){
         return this.currentTime/this.currentSong.duration
       },
+      iconMode(){
+        return this.mode===0? 'icon-sequence' :this.mode===1? 'icon-random':'icon-loop'
+      },
       ...mapGetters([
         'currentIndex',
         'playlist',
         'currentSong',
         'fullScreen',
-        'playing'
+        'playing',
+        'mode'
       ])
     },
     created () {
@@ -283,17 +288,42 @@
         }
         return num
       },
+      // 切换模式
+      changeMode(){
+        let currentMode=(this.mode+1)%3
+        this.setPlayMode(currentMode)
+        console.log(this.mode)
+        let list=null
+        if(currentMode===this.mode.random){
+          list=setRandomArr(this.playlist)
+        }else {
+          list=this.playlist
+        }
+        this.setPlaylist(list)
+      },
+      // 从新设置当前index
+      resetCurrentIndex(list){
+        let index=list.findIndex((item)=>{
+          return item.id===this.currentSong.id
+        })
+        this.setCurrentIndex(index)
+      },
       ...mapMutations({
         setFullScreen: 'SET_FULL_SCREEN',  // back 设置 FullScreen
         setPlayingState: 'SET_PLAYING_STATE',
-        setCurrentIndex:'SET_CURRENT_INDEX'
+        setCurrentIndex:'SET_CURRENT_INDEX',
+        setPlayMode:'SET_PLAY_MODE',
+        setPlaylist:'SET_PLAYLIST'
       }),
       ...mapActions([
         'savePlayHistory'
       ])
     },
     watch: {
-      currentSong () {
+      currentSong (newSong,oldSong) {
+        if(newSong.id===oldSong.id){
+          return
+        }
         let _this=this
         this.$nextTick(() => {
           _this.$refs.audio.play()
